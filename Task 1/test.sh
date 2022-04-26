d9ff1c81a9fd77b6549872989a2cae6ee4211fdd
DBprassan12 <divij.bhaw15@gmail.com>
2022-04-26 21:44:18 +0800

    Task1withyears

--- Task 1/test.sh
+++ Task 1/test.sh
@@ -1,17 +1,15 @@
-input=$1
 
 
-grep -w "$input" incedenceOfMalaria.csv
-
-
-
-
--w 
+sortingVariable=$(tail incedenceOfMalaria.csv -n +2 | cut -d, -f3 | sort | uniq )
 
 
 
+rate=$( echo $sortingVariable | tail -n1)
+
+minYear=$(tail incedenceOfMalaria.csv -n +2 | cut -d, -f3 | sort | uniq | head -n1 )
+MaxYear=$(tail incedenceOfMalaria.csv -n +2 | cut -d, -f3 | sort | uniq | tail -n1)
 
 
-
-
--nth
+echo $minYear
+echo $MaxYear
+echo $rate
