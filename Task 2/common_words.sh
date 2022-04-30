64cd6ebbe9e64c00253e623d3cf7c5a37475bbd9
DBprassan12 <divij.bhaw15@gmail.com>
2022-05-01 00:59:37 +0800

    part2workingword2

--- Task 2/common_words.sh
+++ Task 2/common_words.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
 read -p "Enter fileName " fileName
-read -p "Enter Number " number
+read -p "Enter Number " Orignalnumber
 
 
 cat $fileName/*.txt > temp.txt
- number=$(($number+1))
+ number=$(($Orignalnumber+1))
 
 
 Word=$(tr -c '[:alnum:]' '[\n*]' < ./temp.txt | sort | uniq -c | sort -nr | head -n "$number" | tail -1 | cut -d " " -f4)
 
-echo "$Word"
+foundInfiles=$(grep -rl and "$fileName" | wc -l)
 
 
 
-
-
-
+echo The "$Orignalnumber"th most common word is "$Word" across "$foundInfiles" files
 
 
 
