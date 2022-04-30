903a7949d1f24b5da8be620809663a1652376845
DBprassan12 <divij.bhaw15@gmail.com>
2022-05-01 00:42:11 +0800

    part2workingwords

--- Task 2/common_words.sh
+++ Task 2/common_words.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-
-
-
 read -p "Enter fileName " fileName
-read -p "Enter file path " input2
-read -p "Enter file path " input3
+read -p "Enter Number " number
 
-cat "$fileName"/*.txt > temp.txt
 
-tr -c '[:alnum:]' '[\n*]' < ./temp.txt | sort | uniq -c | sort -nr | head -10
+cat $fileName/*.txt > temp.txt
+ number=$(($number+1))
+
+
+Word=$(tr -c '[:alnum:]' '[\n*]' < ./temp.txt | sort | uniq -c | sort -nr | head -n "$number" | tail -1 | cut -d " " -f4)
+
+echo "$Word"
 
 
 
@@ -20,4 +21,3 @@ tr -c '[:alnum:]' '[\n*]' < ./temp.txt | sort | uniq -c | sort -nr | head -10
 
 
 rm temp.txt
-
