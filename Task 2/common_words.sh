c65ee389bd6b1f91c2b17a2924aea17ca775007d
DBprassan12 <divij.bhaw15@gmail.com>
2022-05-01 15:14:16 +0800

    fixedpart2

--- Task 2/common_words.sh
+++ Task 2/common_words.sh
@@ -4,17 +4,55 @@ read -p "Enter fileName " fileName
 read -p "Enter Number " Orignalnumber
 
 
+
 cat $fileName/*.txt > temp.txt
- number=$(($Orignalnumber+1))
+
+number=$(($Orignalnumber+1))
 
 
-Word=$(tr -c '[:alnum:]' '[\n*]' < ./temp.txt | sort | uniq -c | sort -nr | head -n "$number" | tail -1 | cut -d " " -f4)
+# function Nt
 
-foundInfiles=$(grep -rl and "$fileName" | wc -l)
+Word=$(tr -c '[:alnum:]' '[\n*]' < ./temp.txt | 
+sort | uniq -c | sort -nr | head -n "$number" | tail -1 | rev | cut -d' ' -f1 |rev)
+uniqWords=$(tr -c '[:alnum:]' '[\n*]' < ./temp.txt | sort | uniq -c | sort -nr | wc -l )
+foundInfiles=$(grep -rl "$Word" "$fileName" | wc -l)
 
 
 
-echo The "$Orignalnumber"th most common word is "$Word" across "$foundInfiles" files
+
+# function W 
+
+
+
+
+
+
+
+
+
+
+if [[ "$Orignalnumber" =~ ^[+-]?[0-9]+$ ]] &&  (("$Orignalnumber" >= "1" && "$Orignalnumber"  <= "$uniqWords"))
+
+then echo The "$Orignalnumber"th most common word is {"$Word"} across "$foundInfiles" files
+
+else echo bruh
+
+
+
+
+
+
+
+
+fi
+
+
+
+
+
+
+
+
 
 
 
