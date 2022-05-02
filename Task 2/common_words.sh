d980b5e871f540a83a9320fefa9095d186631ac2
DBprassan12 <divij.bhaw15@gmail.com>
2022-05-02 16:43:44 +0800

    ess

--- Task 2/common_words.sh
+++ Task 2/common_words.sh
@@ -5,57 +5,58 @@
 # Assignment 1 2022 class CITS2003
 
 
-cat $fileName/*.txt > temp.txt
-
-uniqWordsCount=$(tr -c '[:alnum:]' '[\n*]' < ./temp.txt | sort | uniq -c | sort -nr | wc -l )
-
-if [[ -d "$1" ]]
-
-then 
-
-FindWord=$(tr -c '[:alnum:]' '[\n*]' < ./temp.txt | sort | uniq -c | sort -nr | head -n2 | tail -1 | rev | cut -d ' ' -f1 | rev )
-foundInfiles=$(grep -rl "$FindWord" "$fileName" | wc -l)
-
-echo "The 1st most common word is {"$FindWord"} across "$foundInfiles" files"
-
-
-if [ "$1" == "-nth" ] &&  (("$2" >= "1" && "$2" <= "$uniqWordsCount"))
+if [[ -d "$1" ]] ;
 
 then
-
-number=$(($2+1))
-
-FindWord=$(tr -c '[:alnum:]' '[\n*]' < ./temp.txt | sort | uniq -c | sort -nr | head -n "$number"| tail -1 | rev | cut -d ' ' -f1 | rev )
-foundInfiles=$(grep -rl "$FindWord" "$fileName" | wc -l)
-
-echo "The "$2"th most common word is {"$FindWord"} across "$foundInfiles" files"
-
-
-
-if [ "$1" == "-w" ] 
-
-then 
-
-filesList=$(grep -rl "$2" "$3")
-
-findRank=$(for FILE in "$3" ;
-
-    do
-        tt=$(tr -c '[:alnum:]' '[\n*]' < $FILE | sort | uniq -c | sort -nr | grep -o -n "$2" | head -n1)
-        echo "$tt" "$FILE"
-    done
-)
-
-findHighestRank=$( echo "$findRank" | sort -nr | tail -n1 | cut -d: -f1)
-findHighestRank=$(($findHighestRank-1))
-findHighestRankedFile=$( echo "$findRank" | sort -nr | tail -n1 | cut -d " " -f2 | cut -d / -f2 )
+    cat $1/*.txt > all.txt
+    
+    FindWord=$(tr -c '[:alnum:]' '[\n*]' < ./all.txt | sort | uniq -c | sort -nr | head -n2 | tail -1 | rev | cut -d ' ' -f1 | rev )
+    foundInfiles=$(grep -rl "$FindWord" "$1" | wc -l)
+    echo "The 1st most common word is {"$FindWord"} across "$foundInfiles" files"
+    
+elif [ "$1" == "-nth" ]
 
 
-echo "The most significant rank for the word "$2" is "$findHighestRank" in file "$findHighestRankedFile""
 
+then
+    
+    cat $3/*.txt > all.txt
+    number=$(($2+1))
+    
+    FindWord=$(tr -c '[:alnum:]' '[\n*]' < ./all.txt | sort | uniq -c | sort -nr | head -n "$number"| tail -1 | rev | cut -d ' ' -f1 | rev )
+    foundInfiles=$(grep -rl "$FindWord" "$3" | wc -l)
+    uniqWordsCount=$(tr -c '[:alnum:]' '[\n*]' < ./all.txt | sort | uniq -c | sort -nr | wc -l )
+    
+    
+    if [[ "$2" =~ ^[+-]?[0-9]+$ ]] &&  (("$2" >= "1" && "$2"  <= "$uniqWordsCount"))
+    then
+        echo "The "$2"th most common word is {"$FindWord"} across "$foundInfiles" files"
+    else
+        echo "invalid input"
+    fi
+    
+    
+elif [ "$1" == "-w" ]
 
+then
+    
+    filesList=$(grep -rl "$2" "$3")
+    findRank=$(for FILE in $filesList;
+        do
+            
+            tt=$(tr -c '[:alnum:]' '[\n*]' < $FILE | sort | uniq -c | sort -nr | grep -n -w "$2"  )
+            echo "$tt $FILE" ;
+            
+    done)
+    
+    findHighestRank=$( echo "$findRank" | sort -nr | tail -n1 | cut -d: -f1)
+    findHighestRank=$(($findHighestRank-1))
+    findHighestRankedFile=$( echo "$findRank" | sort -nr | tail -n1 | rev | cut -d ' ' -f1 | rev | cut -d / -f2  )
+    
+    echo "The most significant rank for the word {"$2"} is {"$findHighestRank"} in file {"$findHighestRankedFile"} "
+    
+    
 else 
-
-echo bruh
-
+    echo "invalid input"
+    
 fi
--- Task 2/test.sh
+++ Task 2/test.sh
@@ -1,7 +1,47 @@
 #!/bin/bash
 
-echo $1
-echo $2
-echo $3 
+
+filesList=$(grep -rl "$1" "$2")
+#echo "$filesList"
 
 
+
+
+findRank=$(for FILE in $filesList;
+do
+
+        tt=$(tr -c '[:alnum:]' '[\n*]' < $FILE | sort | uniq -c | sort -nr | grep -n -w "$1"  )
+        echo "$tt $FILE" ;
+    
+done)
+
+findHighestRank=$( echo "$findRank" | sort -nr | tail -n1 | cut -d: -f1)
+findHighestRank=$(($findHighestRank-1))
+
+
+
+findHighestRankedFile=$( echo "$findRank" | sort -nr | tail -n1 | rev | cut -d ' ' -f1 | rev | cut -d / -f2  )
+
+
+echo "The most significant rank for the word {"$1"} is {"$findHighestRank"} in file {"$findHighestRankedFile"} "
+
+
+
+
+
+
+
+
+
+
+
+#findHighestRank=$( echo "$findRank" | sort -nr | tail -n1 | cut -d: -f1)
+#findHighestRank=$(($findHighestRank-1))
+#findHighestRankedFile=$( echo "$findRank" | sort -nr | tail -n1 | cut -d " " -f2 | cut -d / -f2 )
+
+#echo $findHighestRank
+#echo $findHighestRankedFile
+
+
+
+#echo "The most significant rank for the word "$2" is "$findHighestRank" in file "$findHighestRankedFile""
